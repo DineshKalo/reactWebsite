c09e8f9e1cf3a8941efdcb5d90a81964f7687bd1
DineshKalo <124802914+DineshKalo@users.noreply.github.com>
2024-07-20 23:45:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useRef} from 'react'
-import './App.css'
+import './app.css'
 import Navbar from './Components/Navbar/Navbar'
 import Header from './Components/Header/Header'
 import AboutUs from './Components/AboutUs/AboutUs'
